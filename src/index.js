a28430c88cf60c8607cccb3bcc9099d08cd528d0
Ryan <wrosullivan88@gmail.com>
2022-10-04 23:32:49 -0500

    fresh save!

--- src/index.js
+++ src/index.js
@@ -13,17 +13,18 @@ fetchData();
 
 function renderMessariData(messariData){
     console.log(messariData) // putting this on browser to always have access
-    messariData.data.forEach((assetData) => renderAssetData(assetData))
+    messariData.data.forEach((assetData) => renderAssetTable(assetData))
 };
 
 
 
-function renderAssetData (assetData) {
+function renderAssetTable (assetData) {
     const tableRow = document.createElement('tr') // 'tr' is table row   //create row, attach items to row, attach row to table, then next row
     const tableNameCol = document.createElement('td') // 'td' is table column 
     table.appendChild(tableRow)
     tableNameCol.textContent = assetData.name
     tableRow.appendChild(tableNameCol) //row is parent, column is child
+
 
     const tableSymbol = document.createElement('td')
     const tablePrice = document.createElement('td')
@@ -38,6 +39,8 @@ function renderAssetData (assetData) {
         renderAthData(assetData) // going to include ath data, MC data, market, ROI
         renderMarketCapData(assetData)
         renderMarketData(assetData)
+        renderRoiData(assetData)
+
     };
     
     //const assetName = assetData.name
@@ -51,22 +54,42 @@ cryptoBody.appendChild(table);
 
 // ATH section below - these have to be in top-level scope in order to not print multiple times to DOM
 //const athSection = document.createElement('h2') // !! still need this header to appear: All Time High Data
+//
+
+
+    // table header test
+    // const header = table.createTHead
+    // const row = header.insertRow(0)
+    // // const cell = row.insertCell(0)
+    // cell.innerHTML = "give me head"
+    // let header = table.createTHead
+    // header.textContent = "test"
+
+
+    // // section header test
+    // const athHeader = createElement('h2')
+    // athHeader.textContent = "All Time High Data"
+    // athData.appendChild(athHeader)
+
+
+
 const athPrice = document.createElement('li');
 const athTime = document.createElement('li');
 const athPercentDown = document.createElement('li');
 const athMultiplier = document.createElement('li');
 
 const athData = document.getElementById('ath-data');
+
 athData.appendChild(athPrice);
 athData.appendChild(athTime);
 athData.appendChild(athPercentDown);
 athData.appendChild(athMultiplier);
 
 function renderAthData (assetData) {  
-    athPrice.textContent = `$ ${assetData.metrics.all_time_high.price.toFixed(2)}`
+    athPrice.textContent = `All Time High (ATH): $ ${assetData.metrics.all_time_high.price.toFixed(2)}`  
     athTime.textContent = `Date: ${assetData.metrics.all_time_high.at.split("T")[0]}` 
-    athPercentDown.textContent = `Percent down from ATH: ${assetData.metrics.all_time_high.percent_down.toFixed(2)} %`
-    athMultiplier.textContent = `ATH Multiplier (current price * multiplier = ATH price): ${assetData.metrics.all_time_high.breakeven_multiple.toFixed(2)}`
+    athPercentDown.textContent = `Percent Down from ATH: -${assetData.metrics.all_time_high.percent_down.toFixed(2)} %` // note percent down is postive number. 
+    athMultiplier.textContent = `ATH Multiplier (current price * multiplier = ATH price): ${assetData.metrics.all_time_high.breakeven_multiple.toFixed(3)}`
 
 };
 
@@ -82,9 +105,9 @@ marketCapData.appendChild(dominance);
 marketCapData.appendChild(rank);
 
 function renderMarketCapData (assetData) {
-    marketCap.textContent = `$ ${assetData.metrics.marketcap.current_marketcap_usd.toFixed(2)}`
-    dominance.textContent = `${assetData.metrics.marketcap.marketcap_dominance_percent.toFixed(2)} %`
-    rank.textContent = assetData.metrics.marketcap.rank
+    marketCap.textContent = `Current Market Cap: $ ${assetData.metrics.marketcap.current_marketcap_usd.toFixed(2)}`
+    dominance.textContent = `Market Dominance: ${assetData.metrics.marketcap.marketcap_dominance_percent.toFixed(2)} %`
+    rank.textContent = `Current Rank: ${assetData.metrics.marketcap.rank}`
 };  
 
 // Market Data section below
@@ -99,14 +122,32 @@ marketData.appendChild(twentyFourHourChange);
 marketData.appendChild(twentyFourHourVolume);
 
 function renderMarketData (assetData) {
-    twentyFourHourChange.textContent = `${assetData.metrics.market_data.percent_change_usd_last_24_hours.toFixed(2)} %`
-    twentyFourHourVolume.textContent = `$ ${assetData.metrics.market_data.volume_last_24_hours.toFixed(2)}`
+    twentyFourHourChange.textContent = `24hr Change: ${assetData.metrics.market_data.percent_change_usd_last_24_hours.toFixed(2)} %`
+    twentyFourHourVolume.textContent = `24hr Volume: $ ${assetData.metrics.market_data.volume_last_24_hours.toFixed(2)}`
+
 };
 
 // ROI Data below
 
+const weekChange = document.createElement('li');
+const monthChange = document.createElement('li');
+const yearChange = document.createElement('li');
+const ytdChange = document.createElement('li');
+
+const roiData = document.getElementById('roi-data');
+
+roiData.appendChild(weekChange);
+roiData.appendChild(monthChange);
+roiData.appendChild(yearChange);
+roiData.appendChild(ytdChange);
 
 
+function renderRoiData (assetData) {
+    weekChange.textContent = `Week Change: ${assetData.metrics.roi_data.percent_change_last_1_week.toFixed(2)} %`
+    monthChange.textContent = `Month Change: ${assetData.metrics.roi_data.percent_change_last_1_month.toFixed(2)} %`
+    yearChange.textContent = `Year Change: ${assetData.metrics.roi_data.percent_change_last_1_year.toFixed(2)} %` // endpoint is null for STETH (less than a year old)
+    ytdChange.textContent = `YTD Change: ${assetData.metrics.roi_data.percent_change_year_to_date.toFixed(2)} %`
+};
 
 
 
@@ -114,3 +155,5 @@ function renderMarketData (assetData) {
 
 
 // 'li' bullets are persisting !!
+
+// hover over table, pop up window with link to "add to wallet", alert window "WARNING: NEVER CONNECT YOUR WALLET TO UNTRUSTED SITES"
